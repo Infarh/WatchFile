2f5fa163e11092732207bfd7ce1e69760adbd170
Infarh <shmachilin@yandex.ru>
2020-12-19 18:24:42 +0300

    Добавьте файлы проекта.

---
+++ WatchFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Не указано имя файла");
+    return -1;
+}
+
+var file_name = args[0];
+
+if (!File.Exists(file_name))
+{
+    Console.WriteLine("Файл не найден");
+    return -2;
+}
+
+return 0;
